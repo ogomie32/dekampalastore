b53e871bc8cba9150256a8039baa60f3f436ee5e
Ogochukwu Okoye <o.okoye@gems-consult.com>
2023-05-15 17:28:25 +0100

    change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,10 @@
             <div class="flex items-center space-x-3 font-semibold">
                 <button @click="$router.push('/register')" class="bg-kampala-primary text-white border border-kampala-primary text-sm rounded-md py-3 px-6">Sign up</button>
                 <button @click="$router.push('/login')" class="text-kampala-primary border border-kampala-primary text-sm rounded-md py-3 px-6">Sign in</button>
-                <button class="pl-5"><div><img src="../assets/images/svgs/cart.svg" alt="Cart"></div></button>
+                <div class="flex space-x-1">
+                    <button class="pl-5"><div><img src="../assets/images/svgs/cart.svg" alt="Cart"></div></button>
+                    <div class="text-black text-sm -mt-2 rounded-full w-4 h-4 flex items-center justify-center bg-kampala-primary-light">0</div>
+                </div>
 
             </div>
         </div>
