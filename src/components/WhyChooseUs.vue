322f6c6a4b658d6525f7289f069df482a366f0b5
Ogochukwu Okoye <o.okoye@gems-consult.com>
2023-05-15 17:18:29 +0100

    complete

--- src/components/WhyChooseUs.vue
+++ src/components/WhyChooseUs.vue
@@ -25,14 +25,14 @@
 </template>
 
 <script setup>
-    const data = [
-        {id: 01, title: "High Quality Groceries", text:"We offer  only the best products for our customers, our produce are always fresh and locally sourced and we carry a wide range of organic and natural products to meet all your dietary needs."},
-        {id:02, title: "WIde range of products", text:"We offer a wide range of pantry, staples, household essentials and specialty items to meet all of your grocery needs. We have something for everyone."},
-        {id:03, title: "Great customer service", text: "Our friendly and knowledgeable staff are always ready to help you find what you need and answer all your questions. W e believe in treating our customers like family."},
-        {id:04, title: "Competitive prices", text:"We understand that grocery shopping can be expensive which is why we strive to keep our prices affordable, you will find that out prices are often lower than that of our competitors."},
-        {id:05, title: "Swift delivery", text:"Our Delivery personnel are trained to handle packages with care, ensuring that they are delivered in good condition and in time. Our tracking systems allows clients to monitor the progress of their shipments."},
-        {}
-    ]
+    // const data = [
+    //     {id: 01, title: "High Quality Groceries", text:"We offer  only the best products for our customers, our produce are always fresh and locally sourced and we carry a wide range of organic and natural products to meet all your dietary needs."},
+    //     {id:02, title: "WIde range of products", text:"We offer a wide range of pantry, staples, household essentials and specialty items to meet all of your grocery needs. We have something for everyone."},
+    //     {id:03, title: "Great customer service", text: "Our friendly and knowledgeable staff are always ready to help you find what you need and answer all your questions. W e believe in treating our customers like family."},
+    //     {id:04, title: "Competitive prices", text:"We understand that grocery shopping can be expensive which is why we strive to keep our prices affordable, you will find that out prices are often lower than that of our competitors."},
+    //     {id:05, title: "Swift delivery", text:"Our Delivery personnel are trained to handle packages with care, ensuring that they are delivered in good condition and in time. Our tracking systems allows clients to monitor the progress of their shipments."},
+    //     {id:06, title: "Conveninet online ordering", text: "For those times when you can not make it to the store, you can shop from the comfort of your home and have them delivered to your doorstep."}
+    // ]
 </script>
 
 <style scoped>
