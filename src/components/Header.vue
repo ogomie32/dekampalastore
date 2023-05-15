ecd431d0261480aa03a3d4482bc431b407fba875
Ogochukwu Okoye <o.okoye@gems-consult.com>
2023-05-15 17:09:08 +0100

    cart header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,9 @@
                 <div class="cursor-pointer">Contact Us</div>
             </div>
             <div class="flex items-center space-x-3 font-semibold">
-                <button @click="$router.push('')" class="hover:bg-kampala-primary bg-kampala-primary-light border border-kampala-primary-light text-white text-sm rounded-md py-3 px-6">Add to cart</button>
                 <button @click="$router.push('/register')" class="bg-kampala-primary text-white border border-kampala-primary text-sm rounded-md py-3 px-6">Sign up</button>
                 <button @click="$router.push('/login')" class="text-kampala-primary border border-kampala-primary text-sm rounded-md py-3 px-6">Sign in</button>
+                <button class="pl-5"><div><img src="../assets/images/svgs/cart.svg" alt="Cart"></div></button>
 
             </div>
         </div>
--- src/components/WhyChooseUs.vue
+++ src/components/WhyChooseUs.vue
@@ -25,7 +25,14 @@
 </template>
 
 <script setup>
-    
+    const data = [
+        {id: 01, title: "High Quality Groceries", text:"We offer  only the best products for our customers, our produce are always fresh and locally sourced and we carry a wide range of organic and natural products to meet all your dietary needs."},
+        {id:02, title: "WIde range of products", text:"We offer a wide range of pantry, staples, household essentials and specialty items to meet all of your grocery needs. We have something for everyone."},
+        {id:03, title: "Great customer service", text: "Our friendly and knowledgeable staff are always ready to help you find what you need and answer all your questions. W e believe in treating our customers like family."},
+        {id:04, title: "Competitive prices", text:"We understand that grocery shopping can be expensive which is why we strive to keep our prices affordable, you will find that out prices are often lower than that of our competitors."},
+        {id:05, title: "Swift delivery", text:"Our Delivery personnel are trained to handle packages with care, ensuring that they are delivered in good condition and in time. Our tracking systems allows clients to monitor the progress of their shipments."},
+        {}
+    ]
 </script>
 
 <style scoped>
